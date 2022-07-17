577e9f573ae5ca330312f4c2391b1440aefbd574
AlphaDeltaGamma <92504746+AlphaDeltaGamma@users.noreply.github.com>
2022-07-17 18:16:27 +0530

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,15 +1,3 @@
-def encrypt_char(char, key):
-    return chr(ord('A') + (ord(char) - ord('A') + key) % 26
-def encrypt_message(message, key):
-    message = message.upper()
-    cipher = ''
-    for char in message:
-        if char not in ' ,.':
-            cipher += encrypt_char(char, key)
-        else:
-            cipher += char
-    return cipher
-encrypt_message("you are awesome.", 3)
 def decrypt_char(char, key):
     return chr(ord('A') + (ord(char) - ord('A') + 26 - key) % 26)
 def decrypt_message(cipher, key):
@@ -21,8 +9,7 @@ def decrypt_message(cipher, key):
         else:
             message += char
     return message
-decrypt_message('BRX DUH DZHVRPH.', 3)               
-               
+decrypt_message('BRX DUH DZHVRPH.', 3)    
                
      
      
